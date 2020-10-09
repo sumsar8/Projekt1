23a98765e91b522064adfd9b3d931c642a1db1f3
sumsar8 <rasmus.oberg@elev.ga.ntig.se>
2020-10-09 11:12:00 +0200

    Initial commit

---
+++ oga.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+public class oga {
+    public static void main (String[] args) {
+        JOptionPane.showMessageDialog(null, "Goddagens " +
+                System.getProperty("user.name") +
+                " ha en bra dag! ;)");
+    }
+}
